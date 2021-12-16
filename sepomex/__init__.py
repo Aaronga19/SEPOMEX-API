ebda6f545ce5c4a0f56d6319044490519a506d5e
Aarón Juárez <66045880+Aaronga19@users.noreply.github.com>
2021-12-15 21:24:28 -0600

    base de datos postgres configurada y modelo del usuario

--- sepomex/__init__.py
+++ sepomex/__init__.py
@@ -2,14 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sepomex import routes
 from sepomex.config import settings
-# from flask_bcrypt import Bcrypt
-# from flask_login import LoginManager, login_manager
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager, login_manager
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']= f'sqlite:///{settings.database_name}.db'
+app.config['SQLALCHEMY_DATABASE_URI']= f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 app.config['SECRET_KEY'] = settings.secret_key
 db = SQLAlchemy(app)
-# bcrypt = Bcrypt(app)
-# login_manager= LoginManager(app)
-# login_manager.login_view = 'login'
-# login_manager.login_message_category = 'info'
+
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
