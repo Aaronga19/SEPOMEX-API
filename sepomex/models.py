a6c4818af39da754561f7f4c8a454109b07c302e
Aarón Juárez <66045880+Aaronga19@users.noreply.github.com>
2021-12-16 02:54:19 -0600

    Modelos hechos para archivos json de sepomex

--- sepomex/models.py
+++ sepomex/models.py
@@ -25,3 +25,27 @@ class User(db.Model, UserMixin):
 
     def __repr__(self):
         return f'Item: {self.username}'
+
+
+class State(db.Model):
+    __tablename__ = 'Estados'
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(length=30), nullable=False, unique=True)
+    values = db.relationship('Info', backref="estado_info", lazy=True)
+
+    def __repr__(self):
+        return f'Estado: {self.name}'
+
+
+class Info(db.Model):
+    __tablename__ = 'Informacion'
+    __table_args__ = {'extend_existing': True}
+    codigo_postal = db.Column(db.String(length=5), nullable=False, unique=False)
+    asentamiento = db.Column(db.String(length=30), nullable=False, unique=True, primary_key=True)
+    tipo = db.Column(db.String(length=10), nullable=False, unique=False)
+    estado = db.Column(db.Integer(), db.ForeignKey('Estados.id'))
+    
+    
+    def __repr__(self):
+        return f'Estado: {self.asentamiento}'
